7b0118029e63aae98db9a8b1c3203f189a82f20a
Maria Duvert <maria.duvert@hotmail.com>
2023-03-30 16:31:12 +0000

    get data of one user and display them (+ stat login function)

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -14,9 +14,19 @@ mysql = MySQL(app)
 def hello():
     return "Hello, API!"
 
-@app.route("/login")
+@app.route("/login", methods = ['POST'])
 def login():
-    return "Hello, API!"
+    log = request.form['login']
+    passwd = request.form['password']
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id_user, login FROM Authentification WHERE login = "+log+" and password = "+passwd+";")
+    output = cur.fetchall()
+    users = []
+    for row in output:
+        dict = {'id_user':row[0], 'login':row[1]}
+        users.append(dict)
+    cur.close()
+    return jsonify(users)
 
 @app.route("/identity")
 def identity():
@@ -30,14 +40,12 @@ def identity():
     cur.close()
     return jsonify(users)
 
-@app.route("/identity/<id>")
-def userDetail(id):
+@app.route("/identity/<id_user>")
+def userDetail(id_user):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Utilisateur WHERE id_user = id;")
-    output = cur.fetchall()
-    users = []
-    for row in output:
-        dict = {'id_user':row[0], 'login':row[1]}
-        users.append(dict)
+    cur.execute("SELECT id_user,nom,prenom,naissance FROM Utilisateur WHERE Utilisateur.id_user = "+id_user+";")
+    output = cur.fetchone()
+    user = []
+    user.append({'id_user':output[0], 'nom':output[1], 'prenom':output[2], 'naissance':output[3]})
     cur.close()
-    return jsonify(users)
+    return jsonify(user)
--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return render_template('start.html')
+    return render_template('connexion.html')
 
 @app.route("/gestionUser")
 def gestionUser():
@@ -14,7 +14,18 @@ def gestionUser():
     return render_template("gestionUser.html", users=data)
 
 @app.route("/detailUser/<id_user>")
-def detailUser():
-    response = requests.get('https://localhost:5000/identity/', id=id_user)
+def detailUser(id_user):
+    response = requests.get('http://localhost:5000/identity/'+id_user)
     data = response.json()
-    return render_template ("detailUser.html", user=data)
+    return render_template("detailUser.html", user=data)
+    
+"""
+@app.route("/login")
+def gestionUser():
+    response = requests.get('http://localhost:5000/login')
+    data = response.json()
+    if data == True :
+        return render_template("start.html")
+    else :
+        return render_template("connexion.html", msg="Connexion échouée.")
+"""
